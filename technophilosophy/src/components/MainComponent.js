06a2f38eb07a5850573835a0f6d9ad944f372666
Rocky Munoz <rmunoz@churchcommunitybuilder.com>
2019-12-14 12:33:25 -0700

    getting a basic something up and running

--- technophilosophy/src/components/MainComponent.js
+++ technophilosophy/src/components/MainComponent.js
@@ -3,7 +3,7 @@ import Header from './HeaderComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { POSTS } from '../shared/posts';
 import { VIDEOS } from '../shared/videos';
-import { GAMES } from '../shared/games';
+// import { GAMES } from '../shared/games';
 
 
 class Main extends Component {
@@ -11,38 +11,16 @@ class Main extends Component {
         super(props);
         this.state = {
             posts: POSTS,
-            videos: VIDEOS,
-            games: GAMES 
+            videos: VIDEOS
+            // games: GAMES 
         };
     }
 
     render() {
-
-        const HomePage = () => {
-            return (
-                <Home 
-                    post={this.state.posts.filter(post => post.featured)[0]}
-                    video={this.state.videos.filter(video => video.featured)[0]}
-                    game={this.state.games.filter(game => game.featured)[0]}
-                />
-            );
-        }
-
         return (
-            <div>
-                <Header />
-                <Switch>
-                    <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
-                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} /> } />
-                    <Route exact path='/contactus' component={Contact} />
-                    <Redirect to='/home' />
-                </Switch>
-                <Footer />
-            </div>
+                <h1>Rocky's site</h1>
         );
-    }
+    }   
 }
 
 export default Main;
